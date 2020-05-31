af6b655cffca4c324adf944e078fa918171399bd
lindf <1758765238@qq.com>
2020-05-31 21:16:01 +0800

    update wxsign2

--- app/controller/common/third.js
+++ app/controller/common/third.js
@@ -13,10 +13,18 @@ class CommonThirdController extends Controller {
     const { ctx } = this
     const { url } = ctx.request.body
     if (url) {
-      const access_token_data = await ctx.curl(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa8c250966f9dda3f&secret=6b92e246287ab25b64aa58ca7e6cfe38`, { dataType: 'json' })
-      const jsapi_ticket_data = await ctx.curl(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token_data.data.access_token}&type=jsapi`, { dataType: 'json' })
-      // console.log(access_token_data.data.access_token)
-      // console.log(jsapi_ticket_data.data.ticket)
+      const access_token_data = await ctx.curl(
+        `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa8c250966f9dda3f&secret=6b92e246287ab25b64aa58ca7e6cfe38`,
+        {
+          dataType: 'json'
+        }
+      )
+      const jsapi_ticket_data = await ctx.curl(
+        `https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token_data.data.access_token}&type=jsapi`,
+        {
+          dataType: 'json'
+        }
+      )
       const noncestr = buildRamStr(16)
       const timestamp = parseInt(new Date() / 1000)
       const signStr = `jsapi_ticket=${jsapi_ticket_data.data.ticket}&noncestr=${noncestr}&timestamp=${timestamp}&url=${url}`
@@ -30,33 +38,6 @@ class CommonThirdController extends Controller {
           url
         }
       })
-      // let nonceStr = buildRamStr(16)
-      // let timestamp = parseInt(new Date() / 1000)
-      // let signature = ''
-      // let access_token_data = await axios.get(`https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wxa8c250966f9dda3f&secret=6b92e246287ab25b64aa58ca7e6cfe38`)
-      // let jsapi_ticket_data = await axios.get(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${access_token_data.data.access_token}&type=jsapi`)
-      // let signStr = `jsapi_ticket=${jsapi_ticket_data.data.ticket}&noncestr=${nonceStr}&timestamp=${timestamp}&url=${url || 'https://www.lindf.com/jssdk.html'}`
-      // // console.log(access_token_data.data)
-      // // console.log(jsapi_ticket_data.data)
-      // // console.log(signStr)
-      // signature = sha1(signStr)
-      // // console.log(signature)
-      // ctx.response.body = configStatus({
-      //   signInfo: {
-      //     appId: 'wxa8c250966f9dda3f',
-      //     timestamp,
-      //     nonceStr,
-      //     signature
-      //   },
-      // })
-      // try {
-      //   let ret = await jwt.verify(ctx.query.token, 'lindf')
-      //   console.log(ret)
-      //   ctx.body = configStatus({ status: 1 })  
-      // } catch (err) {
-      //   ctx.body = configStatus({ status: 2 })
-      // }
-      // return
     }
   }
 }
